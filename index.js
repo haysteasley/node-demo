a9d753e956f1d2e166f1e6aa1a5b083ec59a4390
haysteasley <teasr025@newschool.edu>
2023-11-06 18:32:27 -0500

    adding express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-index.js
+//import express lbrary and assign it o a variable
+import express from 'express'
+
+//create an instance of an express application
+const app = express();
+
+//set the port the application will run
+const port = process.env.PORT || 3001
+
+//set up resonse for the root path of the application
+app.get('/', (req,res) => {
+    res.send("Draw a duck")
+})
+
+//set up the application to listen on the specified post
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
